39039e79a9af2f060e3b2f08b100c4405b6e7f46
Lelouch <shinydark.sky@gmail.com>
2023-03-13 18:18:26 +0700

    confirm

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 #### config credentials
-~/.aws/credentials
-~/.aws/config
+<!-- ~/.aws/credentials
+~/.aws/config -->
 
 # udacicy-cloud-developer-project-4
+To build this project you need to using old version of NodeJS.
+Recommend Node v14.21.3 .
+
+# front-end:
+cd client
+1. npm i 
+
+2. npm start
+
+http://localhost:3000
+
+cd backnend 
+npm i .
+
+export NODE_OPTIONS=--max_old_space_size=8192
+
+
+
+serverless deploy --verbose or sls deploy
+
---
+++ client/src/components/TodoItem.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Button, Checkbox, Divider, Grid, Icon, Image } from 'semantic-ui-react';
+
+export default function TodoItem({todos,onTodoDelete, onEditButtonClick,onTodoCheck}:any) {
+
+    function onClickDelete(id:any,createdAt:any){
+        const confirm = window.confirm("Are you sure delete?");
+        if(confirm)
+            onTodoDelete(id,createdAt)
+    }
+
+    return (
+        <Grid padded>
+            {todos.map((todo:any, pos:any) => {
+                return (
+                    <Grid.Row key={todo.todoId}>
+                        <Grid.Column width={1} verticalAlign="middle">
+                            <Checkbox onChange={() => onTodoCheck(pos)} checked={todo.done} />
+                        </Grid.Column>
+                        <Grid.Column width={10} verticalAlign="middle">
+                            {todo.name}
+                        </Grid.Column>
+                        <Grid.Column width={3} floated="right">
+                            {todo.dueDate}
+                        </Grid.Column>
+                        <Grid.Column width={1} floated="right">
+                            <Button icon color="blue" onClick={() => onEditButtonClick(todo.todoId)}>
+                                <Icon name="pencil" />
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column width={1} floated="right">
+                            <Button
+                                icon
+                                color="red"
+                                onClick={() => onClickDelete(todo.todoId, todo.createdAt)}
+                            >
+                                <Icon name="delete" />
+                            </Button>
+                        </Grid.Column>
+                        {todo.attachmentUrl && <Image src={todo.attachmentUrl} size="small" wrapped />}
+                        <Grid.Column width={16}>
+                            <Divider />
+                        </Grid.Column>
+                    </Grid.Row>
+                );
+            })}
+        </Grid>
+    )
+}
